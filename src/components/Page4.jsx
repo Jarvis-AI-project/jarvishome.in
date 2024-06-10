c507a4df82b1bd9f3152a5ddea1772e3b124db51
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2024-06-11 05:02:56 +0530

    made page3 and page4 responsive

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -1,41 +1,51 @@
 import React from "react";
 
-function Page4(){
-    return (
-      <div className="page4">
-        <h1>Features</h1>
-        <div className="card_container"> 
-          <div className="card">
-            <h2>Device Integration</h2>
-            <div className="card_content">
-              <img  src="/feature1.png" alt="feature"/>
-              <p>Connect each and every one of your devices, be it smart or non-smart using our state-of-the-art firmware board,that can turn your whole house into a smart home.</p>
-            </div>
+function Page4() {
+  return (
+    <div className="page4">
+      <h1>Features</h1>
+      <div className="card_container">
+        <div className="card">
+          <h2>Device Integration</h2>
+          <div className="card_content">
+            <p>
+              Connect each and every one of your devices, be it smart or
+              non-smart using our state-of-the-art firmware board, that can turn
+              your whole house into a smart home.
+            </p>
           </div>
-          <div className="card">
-            <h2>Your Home at Your Fingertips</h2>
-            <div className="card_content">
-              <img src="/feature2.png" alt="feature"/>
-              <p>Our mobile app lets you take control of every device in your home from a simple tap on your mobile screen from anywhere in your house.</p>
-            </div>
+        </div>
+        <div className="card">
+          <h2>Your Home at Your Fingertips</h2>
+          <div className="card_content">
+            <p>
+              Our mobile app lets you take control of every device in your home
+              from a simple tap on your mobile screen from anywhere in your
+              house.
+            </p>
           </div>
-          <div className="card">
-            <h2>Security You Can Trust</h2>
-            <div className="card_content">
-              <img src="/feature3.png" alt="feature"/>
-              <p>Your security is our utmost priority. We use robust security measures for ensuring that your house is as safe as ours.</p>
-            </div>
+        </div>
+        <div className="card">
+          <h2>Security You Can Trust</h2>
+          <div className="card_content">
+            <p>
+              Your security is our utmost priority. We use robust security
+              measures for ensuring that your house is as safe as ours.
+            </p>
           </div>
-          <div className="card">
-            <h2>State-of-the-art AI</h2>
-            <div className="card_content">
-              <img className="small_image" src="/feature4.png" alt="feature"/>
-              <p>Using our speech recognition model, you can control your devices using simple voice commands.</p>
-            </div>
+        </div>
+        <div className="card">
+          <h2>State-of-the-art AI</h2>
+          <div className="card_content">
+            <p>
+              Using our speech recognition model, you can control your devices
+              using simple voice commands.
+            </p>
           </div>
-        </div> 
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Page4;
